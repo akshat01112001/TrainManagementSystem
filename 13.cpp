9fee432536e90ef2879ccb31336a2ca4aeb465df
Akshat Kumar <akshatkumar.2001@gmail.com>
2022-04-09 19:53:20 +0530

    Update 13.cpp

--- 13.cpp
+++ 13.cpp
@@ -9,6 +9,7 @@ using namespace std;
 //delete this later
 class train{
 public:
+	train();
 	ll from;
 	ll to;
 	ll departure_t;
@@ -16,42 +17,64 @@ public:
 	ll num;
 };
 
+train::train(){
+	this.from=0;
+	this.to=0;
+	this.departure_t=0;
+	this.arrival_t=0;
+	this.num=0;
+}
+
+void time(ll &t){
+	if(t%100>59){
+		t-=60;
+		t+=100;
+	}
+	if(t>2359) t-=2400;
+}
+
+void time_updation(ll &t){
+	t+=30;
+}
+
 class station{
 public:
-	priority_queue<ll> plat;
+	void put_train(train *input);
+	void remove_train(train *input);
+private:
+	train* trains[4];
 };
 
-station stat_plat[4][4];
+void station::put_train(train *input){
+	for(int i = 0; i < 4; i++){
+        if(trains[i].from == 0){
+            this->trains[i]=input;
+            cout<< "Train "<<this->trains[i]->num<<" is entering platform no "<<i + 1<<" at "<<this->trains[i]->arrival_t<<" and should leave at "<<this->trains[i]->departure_t<<endl;
+        }
+    }
+}
+
+void Station::remove_train(train *input){
+	for(int i = 0; i < 4; i++){
+        if(trains[i]==input){
+            cout<<"Train "<<this->trains[i]->num<<" is exiting platform "<<i+1<<" at "<<this->trains[i]->departure_t<<endl;
+            trains[i]->from=trains[i]->to=trains[i]->departure_t=trains[i]->arrival_t=trains[i]->num=0;
+        }
+    }
+}
 
 void solve()
 {
 	cout<<"Number of trains: ";
 	ll t,counter=0;
 	cin>>t;
-	vector<train>v(t);
+	vector<train *>v(t);
 	for(auto& tr:v){
 		cout<<"Enter source, destination, incoming time, outgoing time and train number: ";
 		cin>>tr.from>>tr.to>>tr.departure_t>>tr.arrival_t>>tr.num;
 	}
 	sort(v.begin(),v.end());
-	for(auto& tr:v){
-		ll c=INT_MAX;
-		if(tr.from==tr.to) continue;
-		ll i=1;
-		while(!stat_plat[tr.from][i].empty() && i<5)
-			i++;
-		cout<<i<<
-		if(i==5){
-			for(k=1;k<5;k++){
-				train tra=stat_plat[tr.from][k].front();
-				c=min(c,tra.departure_t);
-				if(c==tra.departure_t) i=k;
-			}
-		}
-		stat_plat[tr.from][i].push(tr.num);
-		counter++;
-		if(counter>=c) stat_plat[tr.from++][i].pop();
-	}
+
 
 }
 
