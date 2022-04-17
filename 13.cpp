bf3d9690e02dcca12a1aa0dad37ed3c5aa8e6438
Akshat Kumar <akshatkumar.2001@gmail.com>
2022-04-17 19:47:49 +0530

    Update 13.cpp

--- 13.cpp
+++ 13.cpp
@@ -25,27 +25,16 @@ train::train(){
 	this->num=0;
 }
 
-void time(ll &t){
-	if(t%100>59){
-		t-=60;
-		t+=100;
-	}
-	if(t>2359) t-=2400;
-}
-
-void time_updation(ll &t){
-	t+=30;
-}
-
 class station{
 public:
-	void put_train(train *input);
+	void readout_train(train *input);
 	void remove_train(train *input);
+	void put_train(train *input,vector<train *>v);
 	queue<train *>wait_q;
 	train* plat[4];
 };
 
-void station::put_train(train *input){
+void station::readout_train(train *input){
 	for(int i = 0; i < 4; i++){
         if(plat[i]->from == 0){
             //this->trains[i]=input;
@@ -69,6 +58,49 @@ bool cmp(train * a,train * b) {
 
 //make current time dynamic for transit using while loop
 
+void station::put_train(train* input,vector<train *>v){
+	for(ll i=0;i<4;i++){
+		if(plat[i]->num==0){
+			plat[i]=input;
+			break;
+		}
+	}
+	for(auto& tr:v){
+		ll i=0;
+		while(!plat[i]->num && i<4) i++;
+		plat[i]=tr;
+		if(i==4) wait_q.push(tr);
+	}
+	ll depart=INT_MAX;
+	for(ll j=0;j<4;j++){
+		if(plat[j]->num)
+			depart=min(depart,plat[j]->departure_t);
+	}
+	for(ll j=0;j<4;j++){
+		if(depart==plat[j]->departure_t){
+			plat[j]=wait_q.front();
+			wait_q.pop();
+		}
+	}
+}
+
+void time(ll &t){
+	if(t%100>59){
+		t-=60;
+		t+=100;
+	}
+	if(t>2359) t-=2400;
+}
+
+void time_updation(ll &t,double k){
+	t+=k;
+}
+
+void transit(train *tr,station* stat[],vector<train *>v){
+	stat[tr->from++]->remove_train(tr);
+	if(tr->from<4) stat[tr->from]->put_train(tr,v);
+}
+
 void solve()
 {
 	cout<<"Number of trains: ";
@@ -81,24 +113,25 @@ void solve()
 		cin>>tr->from>>tr->to>>tr->departure_t>>tr->arrival_t>>tr->num;
 	}
 	sort(v.begin(),v.end(),cmp);
-	for(auto& tr:v){
-		i=0;
-		while(!stat[tr->from]->plat[i]->num && i<4) i++;
-		stat[tr->from]->plat[i]=tr;
-		if(i==4) stat[tr->from]->wait_q.push(tr);
-	}
-	ll depart=INT_MAX;
-	for(i=0;i<4;i++){
-		for(j=0;j<4;j++){
-			if(stat[i]->plat[j]->num)
-				depart=min(depart,stat[i]->plat[j]->departure_t);
-			if(depart==stat[i]->plat[j]->departure_t){
-				stat[i]->plat[j]=stat[i]->wait_q.front();
-				stat[i]->wait_q.pop();
+	while(beg_time!=2300){
+		for(auto& tr:v){
+			if(tr->departure_t==beg_time){
+				stat[tr->from]->put_train(tr,v);
+				double k = (tr->arrival_t-tr->departure_t)/(tr->to - tr->from)*1.0;
+				transit(tr,stat,v);
+				stat[tr->from]->readout_train(tr);
+				time_updation(tr->departure_t,k);
 			}
+			// if(tr->arrival_t==beg_time){
+			// 	transit
+			// }
 		}
+		// for(auto& tr:v){
+
+		// }
+		time_updation(beg_time,30);
+		time(beg_time);
 	}
-	
 }
 
 int main()
